adef03376f14376eb27ce14a21a6a4e850704462
Patryk Panek <patryyk221@gmail.com>
2022-08-03 15:26:07 +0200

    refactoring to router@6, added stripe payments, functions from netlify

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 // LZAIjsNkK4SQsgaN6h31JpHQnMU7LBWG
 ReactDOM.render(
   <Auth0Provider
-    domain="dev--yyxnwwy.us.auth0.com"
-    clientId="LZAIjsNkK4SQsgaN6h31JpHQnMU7LBWG"
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
